253903abd041c0fb4756895bac411032c138e9f5
s538093 <74044094+s538093@users.noreply.github.com>
2021-04-06 17:15:31 -0500

    commit1

---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User{
+    constructor(public firstName:String,
+        public lastName:String,public email:String,public phoneNumber:String,public gender:String,public dateOfBirth:String,public country:String,public age:String,public check:String,
+        
+        ){
+        
+    }
+    
+}
